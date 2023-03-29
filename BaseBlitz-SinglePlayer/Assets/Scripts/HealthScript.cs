01c50cc5f6275e4d80f6aa703b16e9d5096b32d9
Alphayama <alphayama@outlook.com>
2023-03-29 17:32:59 -0500

    add unity project files

---
+++ BaseBlitz-SinglePlayer/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    [SerializeField] int health;
+    [SerializeField] AudioSource destroySound;
+    [SerializeField] TextMeshProUGUI healthText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destroySound = GameObject.FindGameObjectWithTag("DestroySound").GetComponent<AudioSource>();
+        //healthText = GameObject.FindGameObjectWithTag("HealthText").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            destroySound.Play();
+            Destroy(gameObject);
+        }
+    }
+
+    public void ReduceHealth(int damage)
+    {
+        health -= damage;
+        healthText.transform.position = new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z);
+        healthText.transform.LookAt(Camera.main.transform);
+        healthText.text = health.ToString();
+        
+    }
+
+    //IEnumerator HealthDisplay()
+    //{
+    //    yield return new WaitForSeconds(1);
+    //}
+}
---
+++ BaseBlitz-SinglePlayer/Assets/Scripts/ShootScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShootScript : MonoBehaviour
+{
+    RaycastHit hit;
+    [SerializeField] AudioSource shootSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shoot() 
+    { 
+        shootSound.Play();
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
+        {
+            if (hit.transform.tag == "MilitaryBaseItem")
+            {
+                hit.transform.GetComponent<HealthScript>().ReduceHealth(10);
+            }
+        }
+    }
+}
