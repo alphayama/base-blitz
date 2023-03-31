01596b8edfa5bdbc2fbdb63627876cad67d6df91
Alphayama <alphayama@outlook.com>
2023-03-31 18:25:09 -0500

    add start menu and script

---
+++ BaseBlitz-SinglePlayer/Assets/Scripts/StartMenuScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ChangeSettings()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
