8cd420007ab936ff5193f0a4465ffe10d820ef8a
ojal21 <ojal_guitar@yahoo.co.in>
2023-04-06 15:27:47 -0500

    game over screen loads for 5 sec then start menu screen appears

---
+++ BaseBlitz-SinglePlayer/Assets/Scripts/GameOverController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverController : MonoBehaviour
+{
+    float timeValue=10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeValue>0){
+                timeValue-=Time.deltaTime;
+            }
+            else{
+                timeValue=0 ;
+
+                //go to game over scene
+                SceneManager.LoadScene("StartMenu");
+               // GameOver();
+            }
+    }
+}
