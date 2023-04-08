e39d487bd06b1a2854028acc444e2bf3a7ace8d2
ojal21 <ojal_guitar@yahoo.co.in>
2023-04-08 17:21:30 -0500

    merge

--- BaseBlitz-SinglePlayer/Assets/Scripts/rayCast.cs
+++ BaseBlitz-SinglePlayer/Assets/Scripts/rayCast.cs
@@ -13,12 +13,17 @@ public class rayCast : MonoBehaviour
 
     LineRenderer laserLine;
 <<<<<<< HEAD
+<<<<<<< HEAD
     bool isRun = false;
     float fireTimer;
 =======
     float fireTimer;
     bool isRun = false;
 >>>>>>> 50e5634 (Try Laser)
+=======
+    bool isRun = false;
+    float fireTimer;
+>>>>>>> bcedc49 (Laser Done)
 
     void Awake()
     {
@@ -27,6 +32,7 @@ public class rayCast : MonoBehaviour
 
     void Update()
     {
+<<<<<<< HEAD
 <<<<<<< HEAD
         Debug.Log("******************In rayCast ************");
         fireTimer += Time.deltaTime;
@@ -37,23 +43,34 @@ public class rayCast : MonoBehaviour
             Debug.Log("******************In rayCast ************");
             fireTimer += Time.deltaTime;
 >>>>>>> 50e5634 (Try Laser)
+=======
+        Debug.Log("******************In rayCast ************");
+        fireTimer += Time.deltaTime;
+        if (isRun) { 
+>>>>>>> bcedc49 (Laser Done)
             fireTimer = 0;
             laserLine.SetPosition(0, laserOrigin.position);
             Vector3 rayOrigin = playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
             RaycastHit hit;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> 50e5634 (Try Laser)
+=======
+>>>>>>> bcedc49 (Laser Done)
             if (Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, gunRange))
             {
                 laserLine.SetPosition(1, hit.point);
                 Destroy(hit.transform.gameObject);
             }
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> 50e5634 (Try Laser)
+=======
+>>>>>>> bcedc49 (Laser Done)
             else
             {
                 laserLine.SetPosition(1, rayOrigin + (playerCamera.transform.forward * gunRange));
